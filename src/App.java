b77382d0037ccc1584a5fa82e47779fb9052bd7d
Mykhailo Balaker <83900885+Angry3vilbot@users.noreply.github.com>
2025-01-19 01:10:17 +0000

    Add FlatLaf Look And Feel

--- src/App.java
+++ src/App.java
@@ -1,7 +1,13 @@
+import com.formdev.flatlaf.FlatLightLaf;
 import javax.swing.*;
 
 public class App implements Runnable {
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel( new FlatLightLaf() );
+        } catch( Exception ex ) {
+            System.err.println( "Failed to initialize LaF" );
+        }
         SwingUtilities.invokeLater(new App());
     }
 
